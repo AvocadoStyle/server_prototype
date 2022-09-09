0377c954f290a536cfe22f518b794051d87b0b0b
Eden <eden.refael@auroralabs.com>
2022-09-09 18:16:06 +0300

    Add project files.

---
+++ mmn14/Server.cpp
@@ -0,0 +1,62 @@
+#include "Server.h"
+#include "MessageProtocol.h"
+
+
+ServerInit::ServerInit() {
+
+	// bind the PORT to the socket and listening on the socket PORT
+	std::cout << "start the server..." << std::endl;
+	sa.sin_addr.s_addr = INADDR_ANY;
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(PORT);
+	bind(s, (struct sockaddr*)&sa, sizeof(sa));
+	listen(s, SOMAXCONN);
+	std::cout << "listening on port " << PORT << std::endl;
+}
+
+void ServerInit::__userConnect() {
+	// accept() - blocking, waiting for client to connect the server.
+	std::cout << "waiting for clients to connect.." << std::endl;
+	this->clientsocket = accept(s, NULL, NULL);
+	std::cout << "client connected!" << std::endl;
+}
+
+void ServerInit::__handlerequest() {
+	// receive the message from the client max len 1024.
+	char clientmsg[1024] = { 0 };
+	recv(this->clientsocket, clientmsg, 1024, 0); 
+	std::cout << "client sent:" << std::endl;
+	std::cout << clientmsg << std::endl;
+
+	// the server will send echo to the client that the message have been recieved.
+	send(this->clientsocket, "Hello from server!", 18, 0);
+
+	Sleep(10000);
+
+	closesocket(this->clientsocket); // closing the socket
+}
+
+void ServerInit::handleclient() {
+	this->__userConnect();
+	//std::thread ct(fn, this->clientsocket);
+	//ct.detach();
+}
+
+
+ServerInit::~ServerInit() {
+	WSACleanup();
+}
+
+
+
+int main() {
+	ServerInit *server;
+	server = new ServerInit();
+
+	while (true) {
+		server->handleclient();
+	}
+
+
+	return 0;
+}
---
+++ mmn14/Server.h
@@ -0,0 +1,45 @@
+#ifndef __SERVER_H__
+#define __SERVER_H__
+#include <thread>
+
+#include <WinSock2.h>
+#include <Windows.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
+
+#include <stdint.h>
+
+#define PORT 8082
+
+
+
+
+
+class ServerInit {
+public:
+	SOCKET clientsocket;
+	WSADATA wsaData;
+	int ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in sa = { 0 };
+	ServerInit();
+	~ServerInit();
+
+	void handleclient();
+	void __handlerequest();
+	void __userConnect();
+
+};
+
+
+
+
+
+
+
+
+
+
+#endif
